4f044fc3ef3ca77c6a3c3d2165e6e8f9411da42f
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-04-05 11:03:20 +0300

    Don't redo extraction when output exist

--- pipeline/200-extract-tiabs.sh
+++ pipeline/200-extract-tiabs.sh
@@ -17,10 +17,41 @@ INDIR="$SCRIPTDIR/../data/pubmed/original_data"
 
 OUTDIR="$SCRIPTDIR/../data/pubmed/texts"
 
+if [[ -z $(find "$INDIR" -name '*.xml.gz') ]]; then
+    echo "$SCRIPT:ABORT: no .xml.gz files found in $INDIR"
+    exit 1
+fi
+
+missing=0
+found=0
+total=0
+files=""
+for f in $(find "$INDIR" -name '*.xml.gz' | sort); do
+    o="$OUTDIR/$(basename "$f" .xml.gz).tar.gz"
+    if [ -e "$o" ]; then
+	echo "$SCRIPT:output exists for $(basename "$f")" >&2
+	found=$((found+1))
+    else
+	echo "$SCRIPT:output for $(basename "$f") to do ..." >&2
+	missing=$((missing+1))
+	if [ "$files" == "" ]; then
+	    files="$f"
+	else
+	    files="$files"$'\n'"$f"
+	fi
+    fi
+    total=$((total+1))
+done
+
+echo "$SCRIPT:output exists for $found/$total ($missing/$total to do)"
+if [ "$missing" -eq 0 ]; then
+    echo "$SCRIPT:nothing to do, exiting."
+    exit 0
+fi
+
 command="$MODULEDIR/extractTIABs.py"
 
-echo "$SCRIPT:running \"$command\" with $PARALLEL_JOBS jobs on data in $INDIR"\
-     >&2
+echo "$SCRIPT:running \"$command\" with $PARALLEL_JOBS jobs on $missing files in $INDIR"
 
-find "$INDIR" -name '*.xml.gz' | sort \
-     | parallel --jobs $PARALLEL_JOBS python3 "$command" -a -z -o "$OUTDIR"
+echo "$files" \
+    | parallel --jobs $PARALLEL_JOBS python3 "$command" -a -z -o "$OUTDIR"
