d0831f57ed3ecc7d6338fa739d241f27f201fbac
Sampo Pyysalo <sampo.pyysalo@gmail.com>
2019-05-17 10:03:17 +0300

    Add parse_standoff, related

--- scripts/standoff.py
+++ scripts/standoff.py
@@ -1,10 +1,17 @@
+from itertools import chain
+
+from logging import error
+
+
 class Textbound(object):
-    def __init__(self, id_, type_, start, end, text):
+    def __init__(self, id_, type_, span, text):
         self.id = id_
         self.type = type_
-        self.start = start
-        self.end = end
+        self.span = span
         self.text = text
+        self.start, self.end = Textbound.parse_span(span)
+        self.normalizations = []
+        self.annset = None
 
     def overlaps(self, other):
         return not (self.end <= other.start or other.end <= self.start)
@@ -21,10 +28,19 @@ class Textbound(object):
                  other.start < self.end < other.end) or
                 (other.start < self.start and
                  self.start < other.end < self.end))
-    
+
+    def add_id_prefix(self, prefix):
+        self.id = '{}:{}'.format(prefix, self.id)
+
+    def remove_id_prefix(self, cascade=True):
+        self.id = self.id.split(':')[-1]
+        if cascade:
+            for n in self.normalizations():
+                n.remove_id_prefix()
+
     def __eq__(self, other):
         return (self.start, self.end, self.type) == (other.start, other.end, other.type)
-    
+
     def __lt__(self, other):
         if self.start != other.start:
             return self.start < other.start
@@ -36,7 +52,7 @@ class Textbound(object):
     def __repr__(self):
         return 'Textbound({}, {}, {}, {}, {})'.format(
             self.id, self.type, self.start, self.end, self.text)
-        
+
     def __str__(self):
         return '{}\t{} {} {}\t{}'.format(
             self.id, self.type, self.start, self.end, self.text)
@@ -56,17 +72,74 @@ class Textbound(object):
     def from_standoff(cls, line):
         id_, type_span, text = line.split('\t')
         type_, span = type_span.split(' ', 1)
-        start, end = cls.parse_span(span)
-        return cls(id_, type_, start, end, text)
+        return cls(id_, type_, span, text)
 
 
 class Normalization(object):
-    def __init__(self, id_, tb_id, norm_id, text):
+    def __init__(self, id_, type_, tb_id, norm_id, text):
         self.id = id_
+        self.type = type_
         self.tb_id = tb_id
         self.norm_id = norm_id
         self.text = text
+        self.annset = None
+
+    def add_id_prefix(self, prefix):
+        self.id = '{}:{}'.format(prefix, self.id)
+        self.tb_id = '{}:{}'.format(prefix, self.tb_id)
+
+    def remove_id_prefix(self):
+        self.id = self.id.split(':')[-1]
+        self.tb_id = self.id.split(':')[-1]
 
     def __str__(self):
-        return '{}\tReference {} {}\t{}'.format(
-            self.id, self.tb_id, self.norm_id, self.text)
+        return '{}\t{} {} {}\t{}'.format(self.id, self.type, self.tb_id,
+                                         self.norm_id, self.text)
+
+    @classmethod
+    def from_standoff(cls, line):
+        id_, type_ids, text = line.split('\t')
+        type_, tb_id, norm_id = type_ids.split(' ')
+        return cls(id_, type_, tb_id, norm_id, text)
+
+
+def parse_standoff(ann, source='<INPUT>', annset=None):
+    # Note: only handles textbounds and normalizations
+    textbounds = []
+    normalizations = []
+    for ln, l in enumerate(ann.splitlines(), start=1):
+        if not l or l.isspace():
+            continue
+        elif l[0] == 'T':
+            try:
+                textbounds.append(Textbound.from_standoff(l))
+            except Exception as e:
+                error('line {} in {}: {}'.format(ln, source, l))
+                raise
+        elif l[0] == 'N':
+            try:
+                normalizations.append(Normalization.from_standoff(l))
+            except Exception as e:
+                error('line {} in {}: {}'.format(ln, source, l))
+                raise
+        else:
+            warning('skipping line {} in {}: {}'.format(ln, source, l))
+            continue
+
+    # Attach normalizations to textbounds
+    tb_by_id = {}
+    for t in textbounds:
+        tb_by_id[t.id] = t
+    for n in normalizations:
+        tb = tb_by_id.get(n.tb_id)
+        if tb is not None:
+            tb.normalizations.append(n)
+        else:
+            error('skip normalization for unknown textbound: {}'.format(n))
+
+    if annset is not None:
+        for a in chain(textbounds, normalizations):
+            a.add_id_prefix(annset)
+            a.annset = annset
+
+    return textbounds
